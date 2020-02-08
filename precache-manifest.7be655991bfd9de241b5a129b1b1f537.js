ed372bf8c3d401015d06d1723e5a335cce08f2ec
Akshay Jain <akshay.jain@instarem.com>
2020-02-08 15:48:12 +0530

    Updates

---
+++ precache-manifest.7be655991bfd9de241b5a129b1b1f537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5be9f28a09eca806d453e2d3bdd92a",
+    "url": "/tickTackToe/index.html"
+  },
+  {
+    "revision": "543f1cc10b47afae67ea",
+    "url": "/tickTackToe/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8fbe6dd632ac442e606c",
+    "url": "/tickTackToe/static/js/2.3691f456.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tickTackToe/static/js/2.3691f456.chunk.js.LICENSE"
+  },
+  {
+    "revision": "543f1cc10b47afae67ea",
+    "url": "/tickTackToe/static/js/main.70c40bfa.chunk.js"
+  },
+  {
+    "revision": "0d9788b22d1bb539ee8e",
+    "url": "/tickTackToe/static/js/runtime-main.4d31d08f.js"
+  }
+]);
